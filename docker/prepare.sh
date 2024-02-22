555259d92e5637b8c7d3387f099ebc42048f5703
Gyumin Hwangbo <ghkdqhrbals@gmail.com>
2024-02-23 07:13:39 +0900

    Add mkdir and *.war check process

--- docker/prepare.sh
+++ docker/prepare.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
-echo "copying ngrinder-controller"
-mkdir -p controller/binary
-rm -rf controller/binary/ngrinder*
-cp ../ngrinder-controller/build/libs/*.war controller/binary/
+CONTROLLER_BINARY_DIR="controller/binary"
+NGRINDER_CONTROLLER_BUILD_DIR="../ngrinder-controller/build/libs"
+NGRINDER_WAR_FILES="${NGRINDER_CONTROLLER_BUILD_DIR}/*.war"
+
+echo "Checking ngrinder-controller war file"
+if ls ${NGRINDER_WAR_FILES} 1> /dev/null 2>&1; then
+    echo "Copying ngrinder-controller war file to ${CONTROLLER_BINARY_DIR}"
+    mkdir -p ${CONTROLLER_BINARY_DIR}
+    for war_file in ${NGRINDER_WAR_FILES}; do
+        cp -n ${war_file} ${CONTROLLER_BINARY_DIR}/
+    done
+else
+    echo "No ngrinder-controller war file found. Skipping the copying process."
+fi
+
+echo "Create directory for ngrinder-controller"
+mkdir -p ~/ngrinder-controller
+chmod -R 777 ~/ngrinder-controller
